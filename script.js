1846b547cab61243c6a646170aa3a490ad6cefc2
Adam First <39226308+prim4t@users.noreply.github.com>
2021-01-10 20:48:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//RELOAD
+    function reload_iframes() {
+        var f_list = document.getElementsByTagName('iframe');
+        for (var i = 0, f; f = f_list[i]; i++) {
+            f.src = f.src;
+        }
+    }
 
 
 //GENERATE GRID
